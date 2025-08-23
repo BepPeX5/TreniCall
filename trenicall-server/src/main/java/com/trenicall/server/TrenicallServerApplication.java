af2f8df50604aa7cdc006ca035f3dd28739b19a4
beppex <giucur@gmail.com>
2025-08-23 12:30:18 +0200

    Primo commit

---
+++ trenicall-server/src/main/java/com/trenicall/server/TrenicallServerApplication.java
@@ -0,0 +1,13 @@
+package com.trenicall.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenicallServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenicallServerApplication.class, args);
+	}
+
+}
---
+++ trenicall-server/src/test/java/com/trenicall/server/TrenicallServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trenicall.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenicallServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
